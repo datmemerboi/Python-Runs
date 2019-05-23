fdfe952a31ffed0473f4d27bf9c59cfd6034d902
datmemerboi <shravanssd@gmail.com>
2019-05-23 23:42:26 +0530

    Random Number Generator

---
+++ AlphaNumGen.py
@@ -0,0 +1,7 @@
+import string, random
+print( random.choice( string.digits+string.ascii_uppercase )+
+random.choice( string.digits+string.ascii_uppercase )+
+random.choice( string.digits+string.ascii_uppercase )+
+random.choice( string.digits+string.ascii_uppercase )+
+random.choice( string.digits+string.ascii_uppercase ) )
+# print(string.digits+string.ascii_uppercase)
