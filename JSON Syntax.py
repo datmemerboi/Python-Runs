0d1fa5093c70146cf4dac771acc43f6b8f29d4cf
datmemerboi <shravanssd@gmail.com>
2019-07-27 18:29:36 +0530

    Second Commit

--- JSON Syntax.py
+++ JSON Syntax.py
@@ -1,11 +1,13 @@
 # JSON Syntax
 InpStr = input("Enter string to check: ")
-balanced = 0
+balanced = depth = 0
 
 for i in range(0, len(InpStr)):
 	if(InpStr[i]=='[' or InpStr[i]=='{'):
 		balanced += 1
+		depth += 1
 	if(InpStr[i]==']' or InpStr[i]=='}'):
 		balanced -= 1
 		
 print( not(bool(balanced)) )
+print(depth)
